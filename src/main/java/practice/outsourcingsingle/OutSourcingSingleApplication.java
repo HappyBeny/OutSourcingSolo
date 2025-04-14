93b3ea5df83f58adad25beaeb9cfee8cd6bfac10
Beny <qkdehdgur3@gmail.com>
2025-04-14 17:23:04 +0900

    아웃소싱 클론코딩 시작

---
+++ src/main/java/practice/outsourcingsingle/OutSourcingSingleApplication.java
@@ -0,0 +1,13 @@
+package practice.outsourcingsingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutSourcingSingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutSourcingSingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practice/outsourcingsingle/OutSourcingSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.outsourcingsingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutSourcingSingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
